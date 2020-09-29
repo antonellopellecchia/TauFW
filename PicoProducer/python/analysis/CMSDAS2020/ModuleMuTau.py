b4277edc0db29a0268a6b936657f89b8b2360c6a
Antonello Pellecchia <antonello.pellecchia@cern.ch>
2020-09-29 14:23:58 +0200

    Add muon and electron veto, jet selection, met, puppimet and transverse mass of muon+MET system

--- PicoProducer/python/analysis/CMSDAS2020/ModuleMuTau.py
+++ PicoProducer/python/analysis/CMSDAS2020/ModuleMuTau.py
@@ -5,6 +5,7 @@ from ROOT import Math
 import numpy as np
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection, Object
+from TauFW.PicoProducer.analysis.utils import deltaPhi
 
 
 # Inspired by 'Object' class from NanoAODTools.
@@ -84,6 +85,46 @@ class ModuleMuTau(Module):
     self.tree.Branch('decayMode_2',  self.decayMode_2, 'decayMode_2/I')
     self.tree.Branch('m_vis',        self.m_vis, 'm_vis/F'            )
     self.tree.Branch('genWeight',    self.genWeight,   'genWeight/F'  )
+
+    # additional quantities
+    self.n_jets = np.zeros(1,dtype='f')
+    self.leading_jet_pt = np.zeros(1,dtype='f')
+    self.subleading_jet_pt = np.zeros(1,dtype='f')
+    self.n_jets_b = np.zeros(1,dtype='f')
+    self.leading_jet_b_pt = np.zeros(1,dtype='f')
+    self.leading_jet_b_eta = np.zeros(1,dtype='f')
+    self.subleading_jet_b_pt = np.zeros(1,dtype='f')
+    self.subleading_jet_b_eta = np.zeros(1,dtype='f')
+    self.met_pt = np.zeros(1,dtype='f')
+    self.met_phi = np.zeros(1,dtype='f')
+    self.met_sum_et = np.zeros(1,dtype='f')
+    self.puppimet_pt = np.zeros(1,dtype='f')
+    self.puppimet_phi = np.zeros(1,dtype='f')
+    self.puppimet_sum_et = np.zeros(1,dtype='f')
+
+    self.tree.Branch('n_jets', self.n_jets, 'n_jets/F')
+    self.tree.Branch('leading_jet_pt', self.leading_jet_pt, 'leading_jet_pt/F')
+    self.tree.Branch('subleading_jet_pt', self.subleading_jet_pt, 'subleading_jet_pt/F')
+    self.tree.Branch('n_jets_b', self.n_jets_b, 'n_jets_b/F')
+    self.tree.Branch('leading_jet_b_pt', self.leading_jet_b_pt, 'leading_jet_b_pt/F')
+    self.tree.Branch('leading_jet_b_eta', self.leading_jet_b_eta, 'leading_jet_b_eta/F')
+    self.tree.Branch('subleading_jet_b_pt', self.subleading_jet_b_pt, 'subleading_jet_b_pt/F')
+    self.tree.Branch('subleading_jet_b_eta', self.subleading_jet_b_eta, 'subleading_jet_b_eta/F')
+    self.tree.Branch('met_pt', self.met_pt, 'met_pt/F')
+    self.tree.Branch('met_phi', self.met_phi, 'met_phi/F')
+    self.tree.Branch('met_sum_et', self.met_sum_et, 'met_sum_et/F')
+    self.tree.Branch('puppimet_pt', self.puppimet_pt, 'puppimet_pt/F')
+    self.tree.Branch('puppimet_phi', self.puppimet_phi, 'puppimet_phi/F')
+    self.tree.Branch('puppimet_sum_et', self.puppimet_sum_et, 'puppimet_sum_et/F')
+
+    self.r_mu_tau    = np.zeros(1, dtype='f')
+    self.mt_1        = np.zeros(1, dtype='f')
+    self.mt_2        = np.zeros(1, dtype='f')
+    
+    self.tree.Branch('r_mu_tau',     self.r_mu_tau, 'r_mu_tau/F'      )
+    self.tree.Branch('mt_1',        self.mt_1, 'mt_1/F'            )
+    self.tree.Branch('mt_2',        self.mt_2, 'mt_2/F'            )
+
   
   def endJob(self):
     """Wrap up after running on all events and files"""
@@ -109,7 +150,7 @@ class ModuleMuTau(Module):
     for muon in Collection(event,'Muon'):
       good_muon = muon.mediumId and muon.pfRelIso04_all < 0.5 and abs(muon.eta) < 2.5
       signal_muon = good_muon and muon.pt > 28.0
-      veto_muon   = False # TODO section 4: introduce a veto muon selection here
+      veto_muon   = good_muon and muon.pt > 15.0
       if signal_muon:
         muons.append(muon)
       if veto_muon: # CAUTION: that's NOT an elif here and intended in that way!
@@ -118,6 +159,7 @@ class ModuleMuTau(Module):
     if len(muons) == 0: return False
     self.cutflow.Fill(self.cut_muon)
     # TODO section 4: What should be the requirement to veto events with additional muons?
+    if len(veto_muons) >= 2: return False
     self.cutflow.Fill(self.cut_muon_veto)
     
     # SELECT TAU
@@ -136,7 +178,9 @@ class ModuleMuTau(Module):
     # and a custom isolation cut on PF based isolation using all PF candidates.
     electrons = []
     for electron in Collection(event,'Electron'):
-      veto_electron = False # TODO section 4: introduce a veto electron selection here
+      veto_electron = electron.pt > 15.0
+      veto_electron = veto_electron and electron.mvaFall17V2noIso_WPL
+      veto_electron = veto_electron and electron.pfRelIso03_all < 0.3
       if veto_electron:
         electrons.append(electron)
     if len(electrons) > 0: return False
@@ -158,6 +202,23 @@ class ModuleMuTau(Module):
     # The collected jets are furthermore not allowed to overlap with the signal muon and signal tau in deltaR, so selected them to have deltaR >= 0.5 w.r.t. the signal muon and signal tau.
     # Then, select for this collection "usual" jets, which have pt > 30 in addition, count their number, and store pt & eta of the leading and subleading jet.
     # For b-tagged jets, require additionally DeepFlavour b+bb+lepb tag with medium WP and |eta| < 2.5, count their number, and store pt & eta of the leading and subleading b-tagged jet.
+    jets = []
+    jets_b = []
+    for jet in Collection(event, 'Jet'):
+      basic_jet = jet.pt > 20.0 and abs(jet.eta) < 4.7
+      basic_jet = basic_jet and jet.puId > 4 # see https://twiki.cern.ch/twiki/bin/viewauth/CMS/PileupJetID
+      basic_jet = basic_jet and jet.jetId > 1 # see https://twiki.cern.ch/twiki/bin/viewauth/CMS/JetID
+      muon = max(muons,key=lambda p: p.pt)
+      tau  = max(taus,key=lambda p: p.pt)
+      basic_jet = basic_jet and jet.DeltaR(muon) >= 0.5
+      basic_jet = basic_jet and jet.DeltaR(tau) >= 0.5
+    
+      better_jet = basic_jet and jet.pt > 30.0
+      if better_jet: jets.append(jet)
+
+      b_tagged_jet = basic_jet and abs(jet.eta) < 2.5
+      b_tagged_jet = b_tagged_jet and jet.btagDeepFlavB > 0.4184 and jet.btagDeepFlavB <  0.2770
+      if b_tagged_jet: jets_b.append(jet)
 
     # CHOOSE MET definition
     # TODO section 4: compare the PuppiMET and (PF-based) MET in terms of mean, resolution and data/expectation agreement of their own distributions and of related quantities
@@ -190,6 +251,30 @@ class ModuleMuTau(Module):
     self.iso_2[0]       = tau.rawDeepTau2017v2p1VSjet # keep in mind: the HIGHER the value of the discriminator, the more the tau is isolated
     self.decayMode_2[0] = tau.decayMode
     self.m_vis[0]       = (muon.p4()+tau.p4()).M()
+
+    # jet additional quantities
+    self.n_jets[0] = len(jets)
+    if len(jets)>0: self.leading_jet_pt[0] = jets[0].pt
+    if len(jets)>1: self.subleading_jet_pt[0] = jets[1].pt
+    self.n_jets_b[0] = len(jets_b)
+    if len(jets_b)>0: self.leading_jet_b_pt[0] = jets_b[0].pt
+    if len(jets_b)>0: self.leading_jet_b_eta[0] = jets_b[0].eta
+    if len(jets_b)>1: self.subleading_jet_b_pt[0] = jets_b[1].pt
+    if len(jets_b)>1: self.subleading_jet_b_eta[0] = jets_b[1].eta
+
+    # met additional quantities
+    self.met_pt[0] = met.pt
+    self.met_phi[0] = met.phi
+    self.met_sum_et[0] = met.sumEt
+    self.puppimet_pt[0] = puppimet.pt
+    self.puppimet_phi[0] = puppimet.phi
+    self.puppimet_sum_et[0] = puppimet.sumEt
+
+    # additional variables useful for QCD bkg estimation
+    self.r_mu_tau[0]       = muon.DeltaR(tau)
+    self.mt_1[0] = np.sqrt(2*self.pt_1[0]*met.pt*(1-np.cos(deltaPhi(muon.phi,met.phi)) ) )
+    self.mt_2[0] = np.sqrt(2*self.pt_2[0]*met.pt*(1-np.cos(deltaPhi(muon.phi,met.phi)) ) )
+
 
     if self.ismc:
       self.genmatch_1[0]  = muon.genPartFlav # in case of muons: 1 == prompt muon, 15 == muon from tau decay, also other values available for muons from jets
